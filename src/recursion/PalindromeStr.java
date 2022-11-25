64cf47bdc1ee1a3a6da561373ff7a892dcb5632f
sadiul-hakim <sadiulhakim@gmail.com>
2022-11-26 01:18:14 +0600

    Palindrome

--- src/recursion/PalindromeStr.java
+++ src/recursion/PalindromeStr.java
@@ -19,7 +19,27 @@ public class PalindromeStr {
         }
     }
 
+    public static boolean palindrome2(String str, int i) {
+        if (i >= str.length() / 2) {
+            return true;
+
+        }
+
+        if (str.charAt(i) == str.charAt(str.length() - 1 - i)) {
+            /**
+             * If chars matches wail for the end . If we reach to the end that means str is
+             * palindrome
+             */
+            return palindrome2(str, i + 1);
+        } else {
+            // If any char does not match return false
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(palindrome("mmmm", 0, 3));
+        String name = "Hakim";
+        System.out.println(palindrome2("mmmma", 0));
+        System.out.println();
     }
 }
